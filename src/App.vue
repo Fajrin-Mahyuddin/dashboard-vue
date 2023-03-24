df368147a2404faf824c4e821aa8a41b5ea1035d
fajrin <fajrin.telkom12@gmail.com>
2023-03-24 22:57:34 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TestedComponent from './components/TestedComponent.vue';
+const test = "blabla"
+const propsFromParents = "this a props from parents"
+const clickData = () => {
+  console.log("this is at the parents");
+}
+</script>
+
+<template>
+  <div>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld :msg=test />
+  <TestedComponent :title=propsFromParents :fun="clickData" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/TestedComponent.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+	data() {
+		return {
+			text: "this a text"
+		}
+	},
+	props: { title: String, fun: Function },
+}
+</script>
+
+<template>
+	<div>{{ text }} - {{ title }}</div>
+	<button @click="fun && fun()">click button</button>
+</template>
